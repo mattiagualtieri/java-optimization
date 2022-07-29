e443f683d4ba983bfaf8e080ca70da3b0051bf7c
mgualtieri <mgualtieri@doxee.com>
2022-07-29 16:56:08 +0200

    manual benchmark

--- src/main/java/microbenchmark/ManualMicroBenchmark.java
+++ src/main/java/microbenchmark/ManualMicroBenchmark.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class ManualMicroBenchmark {
 
     private static final int SIZE = 1_000;
-    private static final int N_RUNS = 100_000;
+    private static final int N_RUNS = 150_000;
     private static List<Integer> data;
 
     public static void main(String[] args) {
@@ -21,9 +21,7 @@ public class ManualMicroBenchmark {
         }
         double endTime = System.nanoTime();
         double timePerOperation = (endTime - startTime) / (1_000_000_000L * N_RUNS);
-        double timeForExecution = (endTime - startTime) / 1_000_000_000L;
         System.out.println("Result: " + (1 / timePerOperation) + " op/s");
-        System.out.println("Result: " + timeForExecution + " s");
     }
 
     private static void initData() {
