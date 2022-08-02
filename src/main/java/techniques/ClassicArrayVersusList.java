b28836b3711662ab6458f9e2e9a88008469b3ab1
mgualtieri <mgualtieri@doxee.com>
2022-08-02 10:10:05 +0200

    added write methods

--- src/main/java/techniques/ClassicArrayVersusList.java
+++ src/main/java/techniques/ClassicArrayVersusList.java
@@ -13,18 +13,20 @@ import java.util.concurrent.TimeUnit;
 /**
  * The final output with these configurations should be looking something like this:
  *
- * -------------------------------------------------------------------------------
- * |  Benchmark                              Mode  Cnt   Score   Error   Units   |
- * |  ClassicArrayVersusList.readList   thrpt    5   526,556 ± 64,293  ops/ms    |
- * |  ClassicArrayVersusList.readArray  thrpt    5  1134,478 ± 16,180  ops/ms    |
- * -------------------------------------------------------------------------------
+ * ------------------------------------------------------------------------------
+ * |  Benchmark                           Mode  Cnt     Score   Error   Units   |
+ * |  ClassicArrayVersusList.readArray   thrpt       1342,458          ops/ms   |
+ * |  ClassicArrayVersusList.readList    thrpt        676,617          ops/ms   |
+ * |  ClassicArrayVersusList.writeArray  thrpt         27,131          ops/ms   |
+ * |  ClassicArrayVersusList.writeList   thrpt         16,824          ops/ms   |
+ * ------------------------------------------------------------------------------
  *
  */
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 10, time = 1)
-@Measurement(iterations = 5, time = 1)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 1, time = 1)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
 public class ClassicArrayVersusList {
@@ -45,13 +47,33 @@ public class ClassicArrayVersusList {
 
     @Setup
     public void setUp() {
-        dataList = new ArrayList<>(SIZE);
+        dataList = new ArrayList<>();
         dataClassicArray = new int[SIZE];
         for (int i = 0; i < SIZE; i++) {
             int value = new Random().nextInt(Integer.MAX_VALUE);
             dataList.add(value);
             dataClassicArray[i] = value;
         }
+    }
+
+    @Benchmark
+    public void writeList(Blackhole blackhole) {
+        List<Integer> copy = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            int value = new Random().nextInt(Integer.MAX_VALUE);
+            dataList.add(value);
+        }
+        blackhole.consume(copy);
+    }
+
+    @Benchmark
+    public void writeArray(Blackhole blackhole) {
+        int copy[] = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            int value = new Random().nextInt(Integer.MAX_VALUE);
+            copy[i] = value;
+        }
+        blackhole.consume(copy);
     }
 
     @Benchmark
